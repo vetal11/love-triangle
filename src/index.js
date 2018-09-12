e8dd23ee5a4ad67ae0cbf112050bd19b97405ccf
vetal11 <43153451+vetal11@users.noreply.github.com>
2018-09-13 02:03:12 +0300

    !

--- src/index.js
+++ src/index.js
@@ -3,5 +3,20 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var count = 0;
+
+  for (var i = 0; i < preferences.length; i++){
+    if (preferences[i] == 0) continue;
+    var person1 = preferences[i] - 1;
+    if (person1 == i) continue;
+    var person2 = preferences[person1] - 1;
+    if (person1 == person2) continue;
+    var person3 = preferences[person2] - 1;
+    if (person2 == person3) continue;
+    if (person3 == i) {
+      count++;
+    };
+  }
+  
+  return count/3;
 };
